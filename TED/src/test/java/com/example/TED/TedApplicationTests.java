18993d51dcf4b13b77b41484d7e368b09b640071
Mrsceloo <mrscelomedeiros@gmail.com>
2023-09-12 19:09:58 -0300

    Commit Inicial

---
+++ TED/src/test/java/com/example/TED/TedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TED;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
