64ab0e3a40418edf93a6eee676f58b2f0698f308
marcosspablo <marcosspablo0@gmail.com>
2023-11-30 20:59:54 -0300

    ..

--- TED/src/main/java/com/example/TED/authentication/repository/RoleModelRepository.java
+++ TED/src/main/java/com/example/TED/authentication/repository/RoleModelRepository.java
@@ -1,17 +1,4 @@
 package com.example.TED.authentication.repository;
 
-import com.example.TED.authentication.model.RoleModel;
-import com.example.TED.enuns.RoleName;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface RoleModelRepository extends JpaRepository<RoleModel, Long> {
-
-    public List<RoleModel> findByRoleNameIn(List<RoleName> roles);
-
-    public RoleModel findByRoleName(String nome);
-
+public class RoleModelRepository {
 }
