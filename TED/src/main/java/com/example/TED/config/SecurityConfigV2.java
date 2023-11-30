faf0d33abc96f7593859ad17789db6f12a9856d1
marcosspablo <marcosspablo0@gmail.com>
2023-11-30 20:52:11 -0300

    ..

--- TED/src/main/java/com/example/TED/config/SecurityConfigV2.java
+++ TED/src/main/java/com/example/TED/config/SecurityConfigV2.java
@@ -1,6 +1,8 @@
 package com.example.TED.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,6 +28,10 @@ public class SecurityConfigV2 {
                 .defaultSuccessUrl("/swagger-ui/index.html").permitAll()
                 .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
         return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
